7d3b115c0cadffbd6c26dbde8b76da89f7529070
Unknown <kithanu@gmail.com>
2018-07-24 22:02:05 +1000

    Baseline navigation
    Added baseline navigation

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<?php 
-   session_start();
-   if (!(isset($_SESSION['userName']))) {$_SESSION['userName'] = '';}
-   if (!(isset($_SESSION['validUser']))) {$_SESSION['validUser'] = false;}
- ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
    <head>
       <title>Insulated Narwhal: Council Data</title>
@@ -13,22 +8,25 @@
       <link rel="stylesheet" href="style.css" type="text/css"/>
    </head>
    <body>
-      <div id="backround"></div>
+      <div id="background"></div>
       <?php 
       include("php/header.php");
       include("php/navigation.php");
       include("php/footer.php");
-      $ref = trim($_GET["link"]);
-      if (!$ref) {include("php/main.php");}
-      elseif ($ref === "") {include("php/main.php");}
-      elseif ($ref === "main") {include("php/main.php");}
-      elseif ($ref === "council_select") {include("php/council_select.php");}
-      elseif ($ref === "suburb_select") {include("php/suburb_select.php");}
-      elseif ($ref === "council_dataset") {include("php/council_dataset.php");}
-      elseif ($ref === "council_result") {include("php/council_result.php");}
-      elseif ($ref === "result_print") {include("php/result_print.php");}
-      elseif ($ref === "data_types") {include("php/data_types.php");}
-      elseif ($ref === "download") {include("php/download.php");}      
+      if (empty($_GET)) {include("php/main.php");}
+	  else {	
+        $ref = trim($_GET["link"]);
+		if ($ref === "main") {include("php/main.php");}		
+		elseif ($ref === "suburb_select") {include("php/suburb_select.php");}
+		elseif ($ref === "council_dataset") {include("php/council_dataset.php");}
+		elseif ($ref === "council_result") {include("php/council_result.php");}
+		elseif ($ref === "result_print") {include("php/result_print.php");}
+		elseif ($ref === "data_types") {include("php/data_types.php");}
+		elseif ($ref === "download") {include("php/download.php");} 
+		elseif ($ref === "data_analysis") {include("php/data_analysis.php");}  
+		elseif ($ref === "data_visual") {include("php/data_visual.php");}  
+		elseif ($ref === "contact_us") {include("php/contact_us.php");}
+	  }	   
       ?>
    </body>
 </html>
---
+++ php/navigation.php
@@ -0,0 +1,8 @@
+<div id="navbar">
+   <h1>Navigation</h1>
+   <h2 class="menuitem"><a href="index.php?link=main">Home</a></h2>
+   <h2 class="menuitem"><a href="index.php?link=council_select">Council Selection</a></h2>
+   <h2 class="menuitem"><a href="index.php?link=data_analysis">Data Analysis</a></h2>
+   <h2 class="menuitem"><a href="index.php?link=data_visual">Data Visualization</a></h2>
+   <h2 class="menuitem"><a href="index.php?link=contact_us">Contact Us</a></h2>    
+</div>
