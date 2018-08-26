179196e79fc79d0cd29c7414506a0cb3b8d4509b
Ellife <Ellife@users.noreply.github.com>
2018-08-26 17:55:52 +1000

    Add files via upload
    Updated Header and homepage

---
+++ header.php
@@ -0,0 +1,3 @@
+<div class="header header-default" style="display:flex; background-image:url('images/header.png');  background-repeat: no-repeat; background-size: 100% 150px; background-position: right; ">
+<img src="images/2476029_0.jpg" alt="Narwhal" height="150"width="150" >
+</div>
---
+++ main.php
@@ -0,0 +1,14 @@
+<div class="container">
+  <h2>Insulated Narwhal Project</h2>
+  <div class="panel panel-default">
+    <div class="panel-heading">Mission</div>
+    <div class="panel-body">
+		<p>&emsp; An ambitious project that plans to cross analyze data from various different boundaries and give the result based on the users request. Stage one is working implementation of council results with data sources being by postcode or suburb based data. Helping users know basic information regarding services and housing, available in the council selected.</p>
+		<p>&emsp; Currently you can select a council by map selection by using Council Selection menu option, which will yeild a bar graph representation of the selected data. Alternatively you can select a Council by drop down in Data Analysis menu option, and get experimental advanced graph result, which is currently a bubble graph. In this advanced mode you can select some filtered result options when choosing data set.
+		</p>
+
+	</div>
+ 
+
+</div>
+</div>
